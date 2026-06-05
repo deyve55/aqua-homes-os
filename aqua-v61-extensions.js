92fda53cf06467c6cfbf0cf677f4c55e6a603e3d
deyve55 <aquahomesdesign@gmail.com>
2026-06-04 22:54:58 -0400

    Add v61A modular extension loader

---
+++ aqua-v61-extensions.js
@@ -0,0 +1,14 @@
+/*
+ * Aqua Homes OS v61A Modular Extension Loader
+ * Safe empty extension shell for future file-limit recovery work.
+ */
+(function () {
+  'use strict';
+
+  window.AquaV61Extensions = window.AquaV61Extensions || {
+    version: 'v61A',
+    initialized: true
+  };
+
+  console.log('Aqua Homes OS v61A extensions shell loaded.');
+}());
